2eab7a7bfee03b7d462cc433848f238d1c078008
George Stagas <gstagas@gmail.com>
2016-09-26 11:49:05 +0300

    bring back mark

---
+++ src/views/mark.js
@@ -0,0 +1,60 @@
+var dom = require('../../lib/dom');
+var css = require('../style.css');
+var View = require('./view');
+
+module.exports = MarkView;
+
+function MarkView(editor) {
+  View.call(this, editor);
+  this.name = 'mark';
+  this.dom = dom(css.mark);
+}
+
+MarkView.prototype.__proto__ = View.prototype;
+
+MarkView.prototype.use = function(target) {
+  dom.append(target, this);
+};
+
+MarkView.prototype.get = function(range, e) {
+  var mark = e.mark.get();
+  if (range[0] > mark.end.y) return false;
+  if (range[1] < mark.begin.y) return false;
+
+  var offsets = e.buffer.getLineRangeOffsets(range);
+  var area = e.buffer.getAreaOffsetRange(mark);
+  var code = e.buffer.text.getRange(offsets);
+
+  area[0] -= offsets[0];
+  area[1] -= offsets[0];
+
+  var above = code.substring(0, area[0]);
+  var middle = code.substring(area[0], area[1]);
+  var html = e.syntax.entities(above)
+    + '<mark>' + e.syntax.entities(middle) + '</mark>';
+
+  html = html.replace(/\n/g, ' \n');
+
+  return html;
+};
+
+MarkView.prototype.render = function() {
+  if (!this.editor.mark.active) return;
+
+  var page = this.editor.getPageRange([-.5,+.5]);
+  var html = this.get(page, this.editor);
+
+  dom.html(this, html);
+
+  dom.style(this, {
+    top: page[0] * this.editor.char.height,
+    height: 'auto'
+  });
+};
+
+MarkView.prototype.clear = function() {
+  dom.style(this, {
+    top: 0,
+    height: 0
+  });
+};
